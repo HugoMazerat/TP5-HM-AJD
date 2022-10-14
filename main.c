71b0b4c9de02226c79b5cda5a529b7de9c4866e8
Hugo MAZERAT <hugo.mazerat@edu.ece.fr>
2022-10-14 11:22:02 +0200

    version 1 finale hugo

--- main.c
+++ main.c
@@ -20,6 +20,6 @@ int main( )
 
     kruskal(g, adr, listeArc, arbreCouvrant);
     //Je me suis aide du site https://www.programiz.com/dsa/kruskal-algorithm pour coder le programme.
-
+    //lien du github : https://github.com/HugoMazerat/TP5-HM-AJD on a eu quelques problemes avec github, on en a donc cree 2.
 
 }
